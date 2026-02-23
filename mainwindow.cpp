eba462da32a07e7e989b72fe70fd9f1f54c98c4a
Robert <rob.ibr0407@gmail.com>
2026-02-23 22:55:28 +0300

    Qt_02 добавлено рисование траектории по 3 точкам

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,6 +8,9 @@
 #include <QPen>
 #include <QBrush>
 #include <QFont>
+#include <QMessageBox>
+#include <QFile>
+#include <QDir>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -16,44 +19,40 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     int size_of_window = 800;
     resize(size_of_window, size_of_window);
-    setWindowTitle("Подзадачча №0");
+    setWindowTitle("Подзадача №0");
     setWindowFlags(windowFlags() & ~Qt::WindowMaximizeButtonHint);
 
+    //Создаем рабочую область
     QGraphicsScene *workspace = new QGraphicsScene(this);
     workspace->setSceneRect(0, 0, size_of_window, size_of_window);
-
-    // Белый фон
     workspace->setBackgroundBrush(Qt::white);
 
-    // Рисуем оси координат (черные, жирные)
+    // Рисуем оси координат
     QPen axisPen(Qt::black, 2);
     workspace->addLine(0, size_of_window/2, size_of_window, size_of_window/2, axisPen); // ось X
     workspace->addLine(size_of_window/2, 0, size_of_window/2, size_of_window, axisPen); // ось Y
 
-    // Рисуем стрелочки на осях
+    //Рисуем стрелочки на осях координат
     QPen arrowPen(Qt::black, 2);
-    // Стрелка для оси X (справа)
     workspace->addLine(size_of_window-10, size_of_window/2-5, size_of_window, size_of_window/2, arrowPen);
     workspace->addLine(size_of_window-10, size_of_window/2+5, size_of_window, size_of_window/2, arrowPen);
-    // Стрелка для оси Y (сверху) - еще ниже
     workspace->addLine(size_of_window/2-5, 25, size_of_window/2, 15, arrowPen);
     workspace->addLine(size_of_window/2+5, 25, size_of_window/2, 15, arrowPen);
 
-    // Подписи осей
+    // Подписываем оси
     QFont axisLabelFont;
     axisLabelFont.setPointSize(12);
     axisLabelFont.setBold(true);
-
     QGraphicsTextItem *xLabel = new QGraphicsTextItem("X");
     xLabel->setPos(size_of_window-20, size_of_window/2-25);
     xLabel->setFont(axisLabelFont);
     workspace->addItem(xLabel);
-
     QGraphicsTextItem *yLabel = new QGraphicsTextItem("Y");
     yLabel->setPos(size_of_window/2+10, 25);
     yLabel->setFont(axisLabelFont);
     workspace->addItem(yLabel);
 
+    //Функция, рисующая точки
     auto addPointWithLabel = [workspace](int x, int y, QString label, bool isVertical = false) {
         QGraphicsEllipseItem *point = new QGraphicsEllipseItem(x-3, y-3, 6, 6);
         point->setBrush(Qt::black);
@@ -63,48 +62,83 @@ MainWindow::MainWindow(QWidget *parent)
         QGraphicsTextItem *text = new QGraphicsTextItem(label);
 
         if (isVertical) {
-            text->setPos(x + 5, y - 12);  // Для вертикальной оси (подписи справа)
+            text->setPos(x + 5, y - 12);
         } else {
-            text->setPos(x - 12, y + 5);  // Для горизонтальной оси (подписи сверху)
+            text->setPos(x - 12, y + 5);
         }
 
         QFont font;
-        font.setPointSize(8);
+        font.setPointSize(9);
+        font.setBold(true);
         text->setFont(font);
         text->setDefaultTextColor(Qt::black);
         workspace->addItem(text);
     };
 
-    // Горизонтальная ось (X) - подписи сверху
-    for(int i = -(size_of_window/2-50); i <= size_of_window/2-50; i += 50) {
-        if (i != 0) {  // Пропускаем 0, чтобы не накладывался на центр
-            QString qstr = QString::number(i/25);
+    // Рисуем точки на осях
+    int scale = 25;
+    for(int i = -(size_of_window/2-50); i <= size_of_window/2-50; i += 2*scale) {
+        if (i != 0) {
+            QString qstr = QString::number(i/scale);
             addPointWithLabel(i + size_of_window/2, size_of_window/2, qstr, false);
         }
     }
-
-    // Вертикальная ось (Y) - подписи справа
-    for(int i = -(size_of_window/2-50); i <= size_of_window/2-50; i += 50) {
-        if (i != 0) {  // Пропускаем 0, чтобы не накладывался на центр
-            QString qstr = QString::number(-i/25);  // Минус потому что Y в Qt растет вниз
+    for(int i = -(size_of_window/2-50); i <= size_of_window/2-50; i += 2*scale) {
+        if (i != 0) {
+            QString qstr = QString::number(-i/scale);
             addPointWithLabel(size_of_window/2, i + size_of_window/2, qstr, true);
         }
     }
-
-    // Добавляем цифру 0 в центре отдельно
+    //Добавляем 0
     QGraphicsTextItem *zeroLabel = new QGraphicsTextItem("0");
     zeroLabel->setPos(size_of_window/2 + 5, size_of_window/2 + 5);
     QFont zeroFont;
-    zeroFont.setPointSize(9);
+    zeroFont.setPointSize(10);
     zeroFont.setBold(true);
     zeroLabel->setFont(zeroFont);
     workspace->addItem(zeroLabel);
 
-    QGraphicsView *view = new QGraphicsView(workspace, this);
+    // Создаем точки
+    struct PointData {
+        float x;
+        float y;
+        QString name;
+    };
 
+    QVector<PointData> points;
+    points.append({9, 0, "A"});
+    points.append({-7, 5, "B"});
+    points.append({-4, 1, "C"});
+
+    if (points.size() > 0) {
+        QVector<QPointF> screenPoints;
+        for (const PointData &point : points) {
+            //Рисование точек
+            int screenX = size_of_window/2 + point.x * scale;
+            int screenY = size_of_window/2 - point.y * scale;
+            screenPoints.append(QPointF(screenX, screenY));
+            addPointWithLabel(screenX, screenY, point.name);
+        }
+
+        QPen linePen(Qt::black, 2);
+
+        //Рисуем прямые
+        for (int i = 0; i < screenPoints.size() - 1; i++) {
+            QPointF p1 = screenPoints[i];
+            QPointF p2 = screenPoints[i + 1];
+            workspace->addLine(p1.x(), p1.y(), p2.x(), p2.y(), linePen);
+        }
+        if (screenPoints.size() > 2) {
+            QPointF first = screenPoints.first();
+            QPointF last = screenPoints.last();
+            workspace->addLine(last.x(), last.y(), first.x(), first.y(), linePen);
+        }
+    }
+
+    //Улучшение внешнего вида
+    QGraphicsView *view = new QGraphicsView(workspace, this);
     view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-
     setCentralWidget(view);
     view->setRenderHint(QPainter::Antialiasing);
 }
